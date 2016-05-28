eae41af564fe5ea4ecd7fc0f71b4de8760d95904
Denis Romanchuk <den4ikkss@gmail.com>
2016-05-28 04:33:29 +0300

    add python bindings for cpv

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
-modules = Extension('boolean', sources = ['boolean_py.c', 'boolean.c'])
-setup(  name = 'libebuild',
-        version = '1.0',
-        description = 'libebuild',
-        ext_modules = [modules]
-        )
+#modules = Extension('boolean', sources = ['boolean_py.c', 'boolean.c'])
+modules = Extension('cpv', sources = ['cpv_py.c', 'cpv.c'])
+setup(name = 'libebuild',
+      version = '1.0',
+      description = 'libebuild',
+      ext_modules = [modules]
+      )
