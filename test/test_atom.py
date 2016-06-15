79ecd058e1e980740c5cfc31ba6af545c0763e5d
Denis Romanchuk <den4ikkss@gmail.com>
2016-06-15 16:38:05 +0300

    add tests for eapi constraints on atom parsing

--- test/test_atom.py
+++ test/test_atom.py
@@ -8,6 +8,7 @@ sys.path.append(sep.join([prev,builddir]))
 from unittest import TestCase
 from error import InvalidAtom
 from atom import atom
+from functools import partial
 
 valid_atoms = [
 "sys-apps/portage",
@@ -143,7 +144,8 @@ invalid_atoms = [
 "!!=hello/world",
 ]
 
-class ParsingTest(TestCase):
+class TestAtom(TestCase):
+
     def test_random_batch(self):
         for atom_str in invalid_atoms:
             print "%50s <-- invalid" % atom_str
@@ -152,3 +154,105 @@ class ParsingTest(TestCase):
         for atom_str in valid_atoms:
             print "%50s < -- valid" % atom_str
             self.assertIsInstance(atom(atom_str), atom)
+
+    def test_eapi0(self):
+        atom_base = "=cat/pkg-4.1"
+        for postfix in (':1', ':asdf', '[x]', '[x,y]',
+                        ':1[x,y]', '[x,y]:1', ':1::repo'):
+            atom_str = atom_base + postfix
+            self.assertRaises(InvalidAtom, atom, atom_str, eapi=0)
+        self.check_use(0)
+
+    def test_eapi1(self):
+        atom_base = "=cat/pkg-4.1"
+        for postfix in ('[x]', '[x,y]', ':1[x,y]', '[x,y]:1', ':1:repo'):
+            atom_str = atom_base + postfix
+            self.assertRaises(InvalidAtom, atom, atom_str, eapi=1)
+        self.check_use(1)
+
+    def test_eapi2(self):
+        self.check_use(2)
+
+    def test_eapi3(self):
+        self.check_use(3)
+        atom("dev-util/foon:1", eapi=3)
+        atom("dev-util/foon:2", eapi=3)
+        atom("!dev-util/foon:1", eapi=3)
+        atom("dev-util/foon:1[x]", eapi=3)
+        atom("dev-util/foon:1[x?]", eapi=3)
+
+    def test_eapi4(self):
+        self.check_use(4)
+    def test_eapi5(self):
+        self.check_use(5)
+    def test_eapi6(self):
+        self.check_use(6)
+
+
+    def check_use(self, eapi):
+        atom_str = "dev-util/bsdiff"
+        kls = partial(atom, eapi=eapi)
+        if eapi >= 2:
+            # Valid chars: [a-zA-Z0-9_@+-]
+            kls('%s[zZaA09]' % atom_str)
+            kls('%s[x@y]' % atom_str)
+            kls('%s[x+y]' % atom_str)
+            kls('%s[x-y]' % atom_str)
+            kls('%s[x_y]' % atom_str)
+            kls('%s[-x_y]' % atom_str)
+            kls('%s[x?]' % atom_str)
+            kls('%s[!x?]' % atom_str)
+            kls('%s[x=]' % atom_str)
+            kls('%s[!x=]' % atom_str)
+
+            # '.' not a valid char in use deps
+            self.assertRaises(InvalidAtom, kls, "%s[x.y]" % atom_str)
+
+            # Use deps start with an alphanumeric char (non-transitive)
+            self.assertRaises(InvalidAtom, kls, "%s[@x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[_x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[+x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[-@x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[-_x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[-+x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[--x]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, "%s[-]" % atom_str)
+
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[foon")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[[fo]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x][y]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x]:1")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x]a")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[--]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x??]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x=?]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x?=]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x==]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[x??]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[!=]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[!?]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[!!x?]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[!-x?]")
+        else:
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[foo]")
+            self.assertRaises(InvalidAtom, kls, "dev-util/diffball[bar]")
+
+        if eapi >= 4:
+            kls('%s[x(+)]' % atom_str)
+            kls('%s[x(-)]' % atom_str)
+            kls("%s[debug(+)]" % atom_str)
+            kls("%s[debug(-)]" % atom_str)
+            kls("%s[missing(+)]" % atom_str)
+            kls("%s[missing(-)]" % atom_str)
+            kls("%s[missing(+)]" % atom_str)
+            kls("%s[-missing(-)]" % atom_str)
+            kls("%s[-missing(+)]" % atom_str)
+            kls("%s[-missing(-),debug]" % atom_str)
+            kls("%s[-missing(+),debug(+)]" % atom_str)
+            kls("%s[missing(+),debug(+)]" % atom_str)
+            self.assertRaises(InvalidAtom, kls, '%s[x(+-)]' % atom_str)
+            self.assertRaises(InvalidAtom, kls, '%s[x(@)]' % atom_str)
+        else:
+            self.assertRaises(InvalidAtom, kls, '%s[x(+)]' % atom_str)
+            self.assertRaises(InvalidAtom, kls, '%s[x(-)]' % atom_str)
